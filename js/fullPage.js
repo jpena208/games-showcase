cde4031e18f9e6b9f9796e39e4d3c2944d1502d3
jpena208 <49579138+jpena208@users.noreply.github.com>
2022-04-02 02:40:53 -0500

    Uses Bootstrap 5 source files and customizes them with custom scss file using sass compiler and autoprefixer. Uses bootstrap icons. Uses Fullpage.js for full page content and auto scrolling. New fonts and colors. Page is highly responsive. Now need to add the game content and pages.

---
+++ js/fullPage.js
@@ -0,0 +1,68 @@
+var myFullpage = new fullpage("#fullpage", {
+  //Navigation
+  anchors: ["home", "game1", "game2"],
+  menu: "#nav-ul",
+  lockAnchors: false,
+  navigation: true,
+  navigationPosition: "right",
+  //   navigationTooltips: ["home", "game1", "game2"],
+  showActiveTooltip: true,
+  slidesNavigation: false,
+  slidesNavPosition: "bottom",
+
+  //Scrolling
+  css3: true,
+  scrollingSpeed: 1000,
+  autoScrolling: true,
+  fitToSection: true,
+  fitToSectionDelay: 1000,
+  scrollBar: false,
+  easing: "easeInOutCubic",
+  easingcss3: "ease",
+  loopBottom: true,
+  loopTop: true,
+  loopHorizontal: true,
+  continuousVertical: false,
+  continuousHorizontal: false,
+  scrollHorizontally: false,
+  interlockedSlides: false,
+  normalScrollElements: "#element1, .element2",
+  scrollOverflow: false,
+  scrollOverflowReset: false,
+  scrollOverflowOptions: null,
+  touchSensitivity: 15,
+  bigSectionsDestination: null,
+
+  //Accessibility
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
+
+  //Design
+  controlArrows: true,
+  verticalCentered: true,
+  paddingTop: "3em",
+  paddingBottom: "10px",
+  fixedElements: "#header, .footer",
+  responsiveWidth: 0,
+  responsiveHeight: 0,
+  responsiveSlides: false,
+
+  //Custom selectors
+  sectionSelector: ".section",
+  slideSelector: ".slide",
+
+  lazyLoading: true,
+
+  //events
+  onLeave: function (origin, destination, direction) {},
+  afterLoad: function (origin, destination, direction) {},
+  afterRender: function () {},
+  afterResize: function (width, height) {},
+  afterReBuild: function () {},
+  afterResponsive: function (isResponsive) {},
+  afterSlideLoad: function (section, origin, destination, direction) {},
+  onSlideLeave: function (section, origin, destination, direction) {},
+});
+
+fullpage_api.setAllowScrolling(true);
---
+++ js/index.js
@@ -0,0 +1,26 @@
+/* -----------------------------
+    Media Query Listeners
+*/
+// Media query for small screens under 300px
+var mqSmall = window.matchMedia("(max-width: 320px)");
+let icons = document.getElementById("icons");
+let brand = document.getElementById("name");
+let nav = document.getElementById("navbarNav");
+let parent = document.getElementById("navContainer");
+
+// Attach listener function on state changes
+mqSmall.addEventListener("change", mqSmallHandler);
+
+// Places icons inside nav menu on extra small screens
+function mqSmallHandler(mqSmall) {
+  if (mqSmall.matches) {
+    // If media query matches, find icons and move them into nav menu
+    nav.prepend(icons);
+  } else {
+    // If media query doesn't match, find icons and move them back into main
+    parent.insertBefore(icons, brand.nextSibling);
+  }
+}
+
+// Call listener function at run time
+mqSmallHandler(mqSmall);
